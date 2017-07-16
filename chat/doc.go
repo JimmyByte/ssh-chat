edc9bf84a2bdea205ef9b839eea44b798de5605d
JimmyBot <jimmybot@teknik.io>
2017-07-16 09:14:43 -0400

    Update doc.go

--- chat/doc.go
+++ chat/doc.go
@@ -1,6 +1,6 @@
 /*
 `chat` package is a server-agnostic implementation of a chat interface, built
-with the intention of using with the intention of using as the backend for
+with the intention of using as the backend for
 ssh-chat.
 
 This package should not know anything about sockets. It should expose io-style
